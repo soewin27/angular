a3717b503362b9ca1b03bae9ec17b0a68879604c
soe win <soewinhtut27@gmail.com>
2022-07-26 09:49:23 +0630

    This is reactive test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-})
-export class AppComponent {
-  title = 'reactive';
+import { Component, OnInit } from '@angular/core'; import { Observable, of, range, from, fromEvent } from 'rxjs'; 
+import { ajax } from 'rxjs/ajax'; 
+import { filter, map, catchError } from 'rxjs/operators'; 
+import { Product } from './service/product';
+import { ProductService } from './service/product.service';
+@Component({ 
+    selector: 'app-root', 
+    templateUrl: './app.component.html', 
+    styleUrls: ['./app.component.scss'] 
+}) 
+export class AppComponent implements OnInit { 
+    title = 'Reactive programming concept'; 
+    numbers : number[] = []; 
+    val1 : number = 0; 
+    filteredNumbers : number[] = []; 
+    val2 : number = 0; 
+    processedNumbers : number[] = []; 
+    val3 : number = 0; 
+    apiMessage : string=''; 
+    counter : number = 0; 
+    ngOnInit() { 
+      // Observable stream of data Observable<number>
+      // const numbers$ = of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10); 
+      // const numbers$ = range(1,10); 
+      const numbers$ = from([1,2,3,4,5,6,7,8,9,10]); 
+      // observer 
+      const observer = { 
+          next: (num: number) => {this.numbers.push(num); this.val1 += num }, 
+          error: (err: any) => console.log(err), 
+          complete: () => console.log("Observation completed") 
+    }; 
+      numbers$.subscribe(observer); 
+      const filterFn = filter( (num : number) => num > 5 ); 
+      const filteredNumbers = filterFn(numbers$); 
+      filteredNumbers.subscribe( (num : number) => {this.filteredNumbers.push(num); this.val2 += num } ); 
+      const mapFn = map( (num : number) => num + num ); 
+      const processedNumbers$ = numbers$.pipe(filterFn, mapFn); 
+      processedNumbers$.subscribe( (num : number) => {this.processedNumbers.push(num); this.val3 += num } ); 
+      const api$ = ajax({ 
+        url: 'https://httpbin.org/delay/1', 
+        method: 'POST', 
+        headers: {'Content-Type': 'application/text' }, 
+        body: "Hello" 
+      }); 
+      api$.subscribe((res: any) => this.apiMessage = res.response.data);
+      const clickEvent$ = fromEvent(document.getElementById("counter") as HTMLElement, 'click');
+    clickEvent$.subscribe(() => this.counter++);
+  
+    } 
+    products!: Product[];
+    productService;
+  
+    constructor(){
+      this.productService=new ProductService();
+    }
+  
+    getProducts() {
+      
+      this.products=this.productService.getProducts();
+    }
 }

6e469c8d6adcf3542efcb80a95e2911b8a78f717
soe win <soewinhtut27@gmail.com>
2022-08-07 20:33:05 +0630

    commit

--- src/app/pages/tree/tree.component.ts
+++ src/app/pages/tree/tree.component.ts
@@ -13,9 +13,6 @@ export class TodoItemFlatNode {
   expandable: boolean=true;
 }
 
-/**
- * The Json object for to-do list data.
- */
 const TREE_DATA = {
   Groceries: {
     'Almond Meal flour': null,
@@ -30,11 +27,6 @@ const TREE_DATA = {
   Reminders: ['Cook dinner', 'Read the Material Design spec', 'Upgrade Application to Angular'],
 };
 
-/**
- * Checklist database, it can build a tree structured Json object.
- * Each node in Json object represents a to-do item or a category.
- * If a node is a category, it has children items and new items can be added under the category.
- */
 @Injectable()
 export class ChecklistDatabase {
   dataChange = new BehaviorSubject<TodoItemNode[]>([]);
@@ -48,18 +40,12 @@ export class ChecklistDatabase {
   }
 
   initialize() {
-    // Build the tree nodes from Json object. The result is a list of `TodoItemNode` with nested
-    //     file node as children.
     const data = this.buildFileTree(TREE_DATA, 0);
 
-    // Notify the change.
+
     this.dataChange.next(data);
   }
 
-  /**
-   * Build the file structure tree. The `value` is the Json object, or a sub-tree of a Json object.
-   * The return value is the list of `TodoItemNode`.
-   */
   buildFileTree(obj: {[key: string]: any}, level: number): TodoItemNode[] {
     return Object.keys(obj).reduce<TodoItemNode[]>((accumulator, key) => {
       const value = obj[key];
@@ -78,7 +64,7 @@ export class ChecklistDatabase {
     }, []);
   }
 
-  /** Add an item to to-do list */
+
   insertItem(parent: TodoItemNode, name: string) {
     if (parent.children) {
       parent.children.push({item: name} as TodoItemNode);
